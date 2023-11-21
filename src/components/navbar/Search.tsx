9b160e0c86528bca1bd01c5c07ab5c86abf71df2
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-11-21 08:21:31 +0600

    add enter key listener

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 
 type SearchProps = {
 	query: string
@@ -6,12 +6,30 @@ type SearchProps = {
 }
 
 const Search: FC<SearchProps> = ({ query, setQuery }) => {
+	const inputEl = useRef<HTMLInputElement | null>(null)
+
+	useEffect(() => {
+		const callback = (e: KeyboardEvent) => {
+			if (document.activeElement === inputEl.current) return
+
+			if (e.code === 'Enter') {
+				inputEl.current?.focus()
+				setQuery('')
+			}
+		}
+
+		document.addEventListener('keydown', callback)
+
+		return () => document.removeEventListener('keydown', callback)
+	}, [setQuery])
+
 	return (
 		<input
 			className='search'
 			type='text'
 			placeholder='Search movies...'
 			value={query}
+			ref={inputEl}
 			onChange={e => setQuery(e.target.value)}
 		/>
 	)
